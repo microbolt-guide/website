aa1ea0ada931c038d3fc337750627ecc4fc469a5
doitwithnotepad <doitwithnotepad@proton.me>
2024-04-04 17:32:56 +0200

    rainbow mode improvements

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,33 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useMemo } from 'react'
 import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }) {
-    const router = useRouter()
+    const { route, events } = useRouter()
     const hueRef = useRef(0) // Initial hue value
-    const directionRef = useRef(1) // Initial direction
     const intervalIdRef = useRef() // Ref to store interval ID
+
+    const styles = useMemo(() => ({
+        '/system': { hue: '204', sat: '100%' },
+        '/bitcoin': { hue: '33', sat: '94%' },
+        '/lightning': { hue: '52', sat: '98%' },
+        '/faq': { hue: '5', sat: '97%' },
+    }), [])
 
     useEffect(() => {
         const handleRouteChange = (url) => {
-            const styles = {
-                '/system': { hue: '204', sat: '100%' },
-                '/bitcoin': { hue: '33', sat: '94%' },
-                '/lightning': { hue: '52', sat: '98%' },
-                '/faq': { hue: '5', sat: '97%' },
-            }
-
             // Clear any existing interval
             if (intervalIdRef.current) {
                 clearInterval(intervalIdRef.current);
                 intervalIdRef.current = null;
             }
 
-            if (url === '/' || url === '/en-US') {
+            const matchingStyle = Object.entries(styles).find(([route, style]) => url.includes(route));
+
+            if (!matchingStyle) {
                 intervalIdRef.current = setInterval(() => {
-                    hueRef.current += directionRef.current;
+                    hueRef.current++;
                     if (hueRef.current > 360) { // Upper limit
-                        directionRef.current = -1;
-                    } else if (hueRef.current < 0) { // Lower limit
-                        directionRef.current = 1;
+                        hueRef.current = 0; // Reset to lower limit
                     }
                     document.documentElement.style.setProperty(
                         '--nextra-primary-hue',
@@ -38,39 +37,35 @@ export default function MyApp({ Component, pageProps }) {
                         '--nextra-primary-saturation',
                         '100%'
                     );
-                }, 30); // Change hue every 100ms
+                }, 30); // Change hue every 30ms
             } else {
-                for (const [route, style] of Object.entries(styles)) {
-                    if (url.includes(route)) {
-                        document.documentElement.style.setProperty(
-                            '--nextra-primary-hue',
-                            style.hue
-                        );
-                        document.documentElement.style.setProperty(
-                            '--nextra-primary-saturation',
-                            style.sat
-                        );
-                        break;
-                    }
-                }
+                const [, style] = matchingStyle;
+                document.documentElement.style.setProperty(
+                    '--nextra-primary-hue',
+                    style.hue
+                );
+                document.documentElement.style.setProperty(
+                    '--nextra-primary-saturation',
+                    style.sat
+                );
             }
         }
 
         // Call handleRouteChange immediately with the current URL
-        handleRouteChange(router.pathname, router.locale)
+        handleRouteChange(route)
 
         // When the component is mounted, subscribe to router events
-        router.events.on('routeChangeComplete', (url) => handleRouteChange(url, router.locale))
+        events.on('routeChangeComplete', handleRouteChange)
 
         // If the component is unmounted, unsubscribe from the event
         return () => {
-            router.events.off('routeChangeComplete', handleRouteChange)
+            events.off('routeChangeComplete', handleRouteChange)
             // Clear any existing interval when the component is unmounted
             if (intervalIdRef.current) {
-                clearInterval(intervalIdRef.current);
+                clearInterval(intervalIdRef.current)
             }
         }
-    }, [router.events, router.pathname, router.locale])
+    }, [events, route, styles])
 
     return <Component {...pageProps} />
 }
