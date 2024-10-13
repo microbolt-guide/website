375a18205f1f846fd76fe3b473ee20e323d7f5cc
doitwithnotepad <doitwithnotepad@proton.me>
2024-10-13 18:29:00 +0000

    nextra3: bump to 3.0.11 & fix i18n

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,28 @@ import nextra from 'nextra'
 import path from 'path'
 
 const withNextra = nextra({
-	theme: 'nextra-theme-docs',
-	themeConfig: './theme.config.jsx'
-	//defaultShowCopyCode: true,
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+  //defaultShowCopyCode: true,
 })
 
 export default withNextra({
-	i18n: {
-		locales: ['ca', 'en'],
-		defaultLocale: 'ca',
-	},
-	webpack: (config) => {
-		config.resolve.alias['@components'] = path.join(process.cwd(), 'components');
-		return config;
-	},
-	redirects: () => {
-		return [
-			{
-				source: '/docs',
-				destination: '/docs/index',
-				statusCode: 302
-			},
-			{
-				source: '/',
-				destination: '/ca',
-				permanent: true
-			}
-		]
-	},
-	reactStrictMode: true
+  i18n: {
+    locales: ['ca', 'en'],
+    defaultLocale: 'ca',
+  },
+  webpack: (config) => {
+    config.resolve.alias['@components'] = path.join(process.cwd(), 'components');
+    return config;
+  },
+  redirects: () => {
+    return [
+      {
+        source: '/docs',
+        destination: '/docs/index',
+        statusCode: 302
+      }
+    ]
+  },
+  reactStrictMode: true
 })
