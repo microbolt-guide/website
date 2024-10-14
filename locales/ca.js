381db48c96dfda463a849652597beff461805c34
doitwithnotepad <doitwithnotepad@proton.me>
2024-10-14 08:22:28 +0000

    l10n: initial commit and adapt project

---
+++ locales/ca.js
@@ -0,0 +1,11 @@
+export default {
+  LANGUAGE_NAME: 'Català',
+  FEEDBACK: 'APM? Dona\'ns feedback →',
+  DESCRIPTION: 'Construeix el teu propi node complet de Bitcoin. No cal confiar en ningú més.',
+  SEARCH_PLACEHOLDER: 'Cerca a la documentació…',
+  BANNER_TEXT: '🎉 Microbolt 2.0 ja està disponible! Llegeix més →',
+  TOC_TITLE: 'En aquesta pàgina',
+  BACK_TO_TOP: 'Torna a dalt',
+  EDIT_TEXT: 'Edita aquesta pàgina a GitHub →',
+  GIT_TIMESTAMP: 'Última actualització el'
+}
---
+++ locales/en.js
@@ -0,0 +1,11 @@
+export default {
+  LANGUAGE_NAME: 'English',
+  FEEDBACK: 'Question? Give us feedback →',
+  DESCRIPTION: 'Build your own DIY Bitcoin full node. No need to trust anyone else.',
+  SEARCH_PLACEHOLDER: 'Search documentation…',
+  BANNER_TEXT: '🎉 Microbolt 2.0 is out! Read more →',
+  TOC_TITLE: 'On This Page',
+  BACK_TO_TOP: 'Scroll to top',
+  EDIT_TEXT: 'Edit this page on GitHub →',
+  GIT_TIMESTAMP: 'Last updated on'
+}
