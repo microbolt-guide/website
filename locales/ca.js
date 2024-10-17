36ad0c7f15ba0def0f806f9aacb8a7c1fe307515
doitwithnotepad <doitwithnotepad@proton.me>
2024-10-17 11:19:10 +0000

    minor l10n improv

--- locales/ca.js
+++ locales/ca.js
@@ -5,7 +5,7 @@ export default {
   SEARCH_PLACEHOLDER: 'Cerca a la documentació…',
   BANNER_TEXT: '🎉 Microbolt 2.0 ja està disponible! Llegeix més →',
   TOC_TITLE: 'En aquesta pàgina',
-  TOC_L10N_TEXT: 'Ajuda a traduir Microbolt',
+  TOC_L10N_TEXT: 'Ajuda a traduir Microbolt →',
   BACK_TO_TOP: 'Torna a dalt',
   EDIT_TEXT: 'Edita aquesta pàgina a GitHub →',
   GIT_TIMESTAMP: 'Última actualització el'
--- locales/en.js
+++ locales/en.js
@@ -5,7 +5,7 @@ export default {
   SEARCH_PLACEHOLDER: 'Search documentation…',
   BANNER_TEXT: '🎉 Microbolt 2.0 is out! Read more →',
   TOC_TITLE: 'On This Page',
-  TOC_L10N_TEXT: 'Help translate Microbolt',
+  TOC_L10N_TEXT: 'Help translate Microbolt →',
   BACK_TO_TOP: 'Scroll to top',
   EDIT_TEXT: 'Edit this page on GitHub →',
   GIT_TIMESTAMP: 'Last updated on'
