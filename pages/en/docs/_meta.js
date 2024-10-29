65e9ca28496f4b9dbf05b0d06796959f407df53e
doitwithnotepad <doitwithnotepad@proton.me>
2024-10-29 19:42:24 +0000

    Ashigaru: fix build with placeholders

--- pages/en/docs/_meta.js
+++ pages/en/docs/_meta.js
@@ -1,12 +1,12 @@
 export default {
-	index: "Introduction",
-	system: "System",
-	bitcoin: "Bitcoin",
-	"-- More": {
-		title: "More",
-		type: "separator"
-	},
-	ashigaru: "Ashigaru",
-	lightning: "Lightning",
-	nostr: "Nostr"
+  index: "Introduction",
+  system: "System",
+  bitcoin: "Bitcoin",
+  "-- More": {
+    title: "More",
+    type: "separator"
+  },
+  ashigaru: "Ashigaru",
+  lightning: "Lightning",
+  nostr: "Nostr"
 }
