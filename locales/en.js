7d0b73686528568df8ff1f01e568b7805282906d
doitwithnotepad <doitwithnotepad@proton.me>
2024-11-23 10:46:39 +0000

    microbolt: new version numeration

--- locales/en.js
+++ locales/en.js
@@ -3,7 +3,7 @@ export default {
   FEEDBACK: 'Question? Give us feedback →',
   DESCRIPTION: 'Build your own DIY Bitcoin full node. No need to trust anyone else.',
   SEARCH_PLACEHOLDER: 'Search documentation…',
-  BANNER_TEXT: '🎉 Microbolt v2.1 "going dark" is out! Read more →',
+  BANNER_TEXT: '🎉 Microbolt v2.1.0 "going dark" is out! Read more →',
   TOC_TITLE: 'On This Page',
   TOC_L10N_TEXT: 'Help translate Microbolt →',
   BACK_TO_TOP: 'Scroll to top',
