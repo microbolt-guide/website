0e0917ac97d187cb0e5acb9a4fd5107e3d344b49
doitwithnotepad <doitwithnotepad@proton.me>
2024-11-23 15:48:45 +0000

    CLN: hidden guide

--- pages/en/docs/lightning/_meta.js
+++ pages/en/docs/lightning/_meta.js
@@ -1,5 +1,9 @@
 export default {
   lightning_client: "Lightning client",
+  cln: {
+    title: "CLN",
+    display: "hidden"
+  },
   channel_backup: "Channel backup",
   web_app: "Web app",
   mobile_app: "Mobile app"
