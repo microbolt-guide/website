195d729ced5ced998d3f56b560876d61c4a0d81c
doitwithnotepad <doitwithnotepad@proton.me>
2024-12-01 16:20:25 +0000

    Rebase 2/N

--- pages/en/docs/_meta.js
+++ pages/en/docs/_meta.js
@@ -1,11 +1,12 @@
 export default {
-  index: "Introduction",
-  system: "System",
-  bitcoin: "Bitcoin",
-  "-- More": {
-    title: "More",
-    type: "separator"
-  },
-  lightning: "Lightning",
-  nostr: "Nostr"
+	index: "Introduction",
+	system: "System",
+	bitcoin: "Bitcoin",
+	"-- More": {
+		title: "More",
+		type: "separator"
+	},
+	ashigaru: "Ashigaru",
+	lightning: "Lightning",
+	nostr: "Nostr"
 }
