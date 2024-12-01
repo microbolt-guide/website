908c37febc614ad4d9af8ea4b22bd3382613ed8f
doitwithnotepad <doitwithnotepad@proton.me>
2024-12-01 16:22:12 +0000

    Rebase 5/N

--- pages/en/docs/bitcoin/_meta.js
+++ pages/en/docs/bitcoin/_meta.js
@@ -3,5 +3,5 @@ export default {
   electrum_server: "Electrum server",
   desktop_signing_app: "Desktop signing app",
   blockchain_explorer: "Blockchain explorer",
-  mining_pool: "Mining Pool"
+  public_pool: "Public Pool"
 }
