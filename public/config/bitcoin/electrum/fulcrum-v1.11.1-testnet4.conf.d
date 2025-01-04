a00de0bd65dd46ee1dd8c17c0c2b7635f7a690aa
doitwithnotepad <doitwithnotepad@proton.me>
2025-01-04 11:49:00 +0000

    fulcrum: add testnet4

---
+++ public/config/bitcoin/electrum/fulcrum-v1.11.1-testnet4.conf.d
@@ -0,0 +1,5 @@
+FULCRUM_CHAIN=testnet4
+FULCRUM_OPTS='--tcp 127.0.0.1:54001
+              --admin 127.0.0.1:8400
+              --bitcoind 127.0.0.1:48332
+              --rpccookie /run/bitcoind/${FULCRUM_CHAIN}.authcookie'
