7f6299f3f2063b7415a8b9f098e31c4190449bb1
doitwithnotepad <doitwithnotepad@proton.me>
2025-01-05 11:26:17 +0000

    fulcrum: minor improv

--- public/config/bitcoin/electrum/fulcrum.signet.bbo.conf.d
+++ public/config/bitcoin/electrum/fulcrum.signet.bbo.conf.d
@@ -1,5 +1,5 @@
 FULCRUM_CHAIN=signet
-FULCRUM_CONFIGFILE=/etc/fulcrum/signet.bbo.conf
+FULCRUM_CONFIGFILE='/etc/fulcrum/${FULCRUM_CHAIN}.bbo.conf'
 FULCRUM_OPTS='--tcp 127.0.0.1:53001
               --admin 127.0.0.1:8300
               --bitcoind 127.0.0.1:38332
